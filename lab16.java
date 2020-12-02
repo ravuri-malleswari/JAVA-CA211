f12e249ca68fe8b25457ca0b6144cdbb1c79daaa
ravuri-malleswari <71822755+ravuri-malleswari@users.noreply.github.com>
2020-12-02 11:26:16 +0530

    Add files via upload

---
+++ lab16.java
@@ -0,0 +1,24 @@
+import java.util.*;
+final class circle
+{
+ final double PI=3.14;
+ final double r;
+ circle(double r)
+ {
+  this.r=r;
+ }
+ final void area()
+ {
+  double a=r*r*PI;
+  System.out.println(" the area of circle is "+a);
+ }
+}
+class lab16
+{
+ public static void main(String args[])
+ {
+  circle c=new circle(6);
+  c.area();
+ }
+}
+  
